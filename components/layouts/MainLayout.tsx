569e99bd8c60daff8d11d92b7d66a6ad7b25f2ce
jkdrumm <jkdrumm1@gmail.com>
2022-07-14 16:46:54 -0500

    Package upgrades

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -98,7 +98,6 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
   const { data: adminLevel } = useAdminLevel();
   return (
     <Box
-      transition="3s ease"
       bg={useColorModeValue('white', 'gray.900')}
       borderRight="1px"
       borderRightColor={useColorModeValue('gray.200', 'gray.700')}
