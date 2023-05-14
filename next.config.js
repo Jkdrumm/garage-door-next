eac434e9c2818b87034d6cb23bee2690eb7b5bdc
jkdrumm <jkdrumm1@gmail.com>
2023-05-14 02:13:48 -0500

    Trying to fix nextauth pkg runtime issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   output: 'standalone',
-  env: { MONGODB_URI: '127.0.0.1:27017/Garage', NEXTAUTH_URL: 'http://127.0.0.1' },
+  env: { MONGODB_URI: '127.0.0.1:27017/Garage', NEXTAUTH_URL: 'http://127.0.0.1:80' },
   webpack: config => {
     // this will override the experiments
     config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
