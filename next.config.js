5b7c866a4112fdc3a4e3b7ac4dbd11a239b5d8fa
jkdrumm <jkdrumm1@gmail.com>
2023-05-18 14:57:32 -0500

    Removed top level await

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {},
-  experimental: { newNextLinkBehavior: false, appDir: false },
-  webpack: config => {
-    // this will override the experiments
-    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
-    return config;
-  }
+  experimental: { newNextLinkBehavior: false, appDir: false }
 };
 
 module.exports = nextConfig;
